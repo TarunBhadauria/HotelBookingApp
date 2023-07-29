b8ceee2471996bfa911d29ed6def55cf929fc2b1
m.Afnan <m.afnan2018@gmail.com>
2023-07-30 03:59:31 +0530

    index.js is ready

--- Server/index.js
+++ Server/index.js
@@ -1,13 +1,46 @@
 const express = require('express');
-const connectDB = require('./config/database');
-const dotenv = require('dotenv');
-const database = require('./config/database')
-dotenv.config()
 const app = express();
 
+//  Connection with Databse
+const connectDB = require('./configs/database');
+connectDB()
 
-app.listen(4000,()=>{
-    console.log("server is running on 4000");
-});
+//  Importing Middlewares
+const cookieParser = require('cookie-parser');
+const fileUpload = require('express-fileupload');
+const dotEnv = require('dotenv');
 
-database.connect();
+//  Using Middlewares
+app.use(cookieParser());
+app.use(express.json());
+app.use(fileUpload({
+    useTempFiles : true,
+    tempFileDir : '/tmp/',
+    limits: {
+        fileSize: 10*1024*1024,
+    }
+}));
+app.use(
+    cors({
+        origin: process.env.CORS_URL,
+        credentials: true,
+    })
+)
+dotEnv.config();
+
+// Importing routes
+
+//  Assigning Routes
+
+//  Health Check
+app.get('/', (req, res)=>{
+    res.status(200).json({
+        success: true, 
+        message: "Server Started Successfully",
+    })
+})
+
+//  Start Listening
+app.listen(4000, ()=>{
+    console.log('Server Started at port 4000');
+})
