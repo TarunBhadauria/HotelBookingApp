1b5630b97496eb39017d46c210048e1164cf578d
TarunBhadauria <Tarun810singh@gmail.com>
2023-09-08 22:54:24 +0530

    Added color codes and navbar component

--- Client/tailwind.config.js
+++ Client/tailwind.config.js
@@ -3,6 +3,36 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
+    colors: {
+      "black":"",
+      "white":"#ffffff",
+      "hotPink": {
+        25: "#ff385c", // airbnb logo 
+        50: "#ff3b5e", // search icon
+        75:"#e51d4f", // for gradient of login button
+        100:"#d70565" // for gradient of login button
+      },
+      "gray":{
+        5:"#e5e5e5",
+        10:"#f7f7f7",
+        25:"#eeeeee",
+        50:"717171",
+        75:"#afafaf",
+        100:"#b0b0b0",
+        125:"#909090",
+      },
+
+      "richBlack":{
+        25:"#5b5b5b", // for unhighlighted icons below navbar
+        50:"#373737", // Third section of nabar 
+        75:"#262626", // Mid section of navbar
+        100:"#1b1b1b", // for highlighted icons below navbar
+        125:"#232426", // for heading & price of hotel card
+        150:"#222222" // for footer and buttons/icons 
+      }
+
+    }
   },
   plugins: [],
+
 }
