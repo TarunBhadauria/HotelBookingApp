c57b0b28807f3924512e1f5145a3578355afbc25
m.Afnan <m.afnan2018@gmail.com>
2023-09-26 02:20:33 +0530

    Few Changes

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,10 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="font-bold text-3xl text-center">
-     <h1>Welcome</h1>
+      <h1>Welcome</h1>
     </div>
   );
 }
