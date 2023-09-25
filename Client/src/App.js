36c8de04ffa10c28132bca802c072f2f1957a69f
m.Afnan <m.afnan2018@gmail.com>
2023-09-25 22:10:21 +0000

    modified

--- Client/src/App.js
+++ Client/src/App.js
@@ -3,13 +3,8 @@ import Navbar from './Components/Common/Navbar';
 
 function App() {
   return (
-<<<<<<< HEAD
-    <div className="font-bold text-3xl text-center">
-      <h1>Welcome</h1>
-=======
     <div className='h-full w-full bg-[#ffffff]'>
      <Navbar/>
->>>>>>> 12901bfb1dee64121a1eb2607cf350659f3b6bba
     </div>
   );
 }
